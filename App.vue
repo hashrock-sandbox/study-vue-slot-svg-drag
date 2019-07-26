e8d17089e6db1a5187496793edabca93e342851b
hashrock <hashrock@users.noreply.github.com>
2019-07-26 16:17:35 +0900

    fix format

--- App.vue
+++ App.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-
-    
     <svg-canvas v-slot="slotProps">
       <g>
-        <line stroke="black" :x1="points[0].x" :y1="points[0].y" :x2="points[1].x" :y2="points[1].y">
+        <line
+          stroke="black"
+          :x1="points[0].x"
+          :y1="points[0].y"
+          :x2="points[1].x"
+          :y2="points[1].y"
+        />
       </g>
-      <svg-draggable v-for="(point, idx) in points" :key="idx" v-slot="drag" :point="point" @change="updatePoint(point, $event)">
+      <svg-draggable
+        v-for="(point, idx) in points"
+        :key="idx"
+        v-slot="drag"
+        :point="point"
+        @change="updatePoint(point, $event)"
+      >
         <circle fill="white" stroke="black" cx="0" cy="0" :r="10" :selected="drag.selected"></circle>
       </svg-draggable>
     </svg-canvas>
@@ -17,9 +27,9 @@
   </div>
 </template>
 <script>
-import MyComp from "./MyComp.vue"
-import SvgCanvas from "./SvgCanvas.vue"
-import SvgDraggable from "./SvgDraggable.vue"
+import MyComp from "./MyComp.vue";
+import SvgCanvas from "./SvgCanvas.vue";
+import SvgDraggable from "./SvgDraggable.vue";
 export default {
   components: {
     MyComp,
@@ -28,32 +38,27 @@ export default {
   },
   data() {
     return {
-      points: [
-        {x: 100, y: 100},
-        {x: 200, y: 200}
-      ]
+      points: [{ x: 100, y: 100 }, { x: 200, y: 200 }]
     };
   },
   methods: {
-    onChange(payload){
-      console.log(payload)
+    onChange(payload) {
+      console.log(payload);
     },
-    updatePoint(obj, payload){
+    updatePoint(obj, payload) {
       // console.log(obj, payload)
-      obj.x = payload.x
-      obj.y = payload.y
-
+      obj.x = payload.x;
+      obj.y = payload.y;
     }
   }
 };
 </script>
 <style>
-.canv{
+.canv {
   border: 1px solid black;
 }
 
-circle:selected{
+circle:selected {
   fill: gray;
 }
-
 </style>
