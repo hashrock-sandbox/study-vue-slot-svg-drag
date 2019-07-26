41eb2aaaf3924fba14739255603676b7816e08de
hashrock <hashrock@users.noreply.github.com>
2019-07-26 15:24:46 +0900

    add svg base

--- App.vue
+++ App.vue
@@ -3,6 +3,10 @@
     <my-comp v-slot="slotProps">
       This is Slot! ({{slotProps.x}},{{slotProps.y}})
     </my-comp>
+
+    <svg class="canv" width=400 height=400 viewBox="0 0 400 400">
+      <rect x=0 y=0 width=10 height=10>
+    </svg>
   </div>
 </template>
 <script>
@@ -16,3 +20,9 @@ export default {
   }
 };
 </script>
+<style>
+.canv{
+  border: 1px solid black;
+}
+
+</style>
