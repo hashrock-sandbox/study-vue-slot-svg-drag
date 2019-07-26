98ba326c57a476f0712b2ed73506c2b3a7b2868e
hashrock <hashrock@users.noreply.github.com>
2019-07-26 15:21:50 +0900

    feat: add slot

--- App.vue
+++ App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <my-comp v-slot="slotProps">
+      This is Slot! ({{slotProps.x}},{{slotProps.y}})
+    </my-comp>
+  </div>
 </template>
 <script>
+import MyComp from "./MyComp.vue"
 export default {
+  components: {
+    MyComp
+  },
   data() {
     return {};
   }
---
+++ MyComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Hello, MyComp</h1>
+        <p @pointermove="onPointerMove">
+            <slot :x="x" :y="y"></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            x: -1,
+            y: -1
+        }
+    },
+    methods:{
+        onPointerMove(ev){
+            this.x = ev.offsetX;
+            this.y = ev.offsetY;
+        }
+    }
+}
+</script>
+
+<style>
+p{
+    border: 1px solid yellowgreen;
+}
+</style>
